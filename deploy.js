dce53cbfae65728b58d034fa8e949e0f7c17a682
Anisha kumari <swatisingh72777@gmail.com>
2022-03-26 20:03:17 +0530

    make changes in deploy.js

--- deploy.js
+++ deploy.js
@@ -7,8 +7,6 @@ const { interface, bytecode } = require("./compile");
 const provider = new HDWalletProvider(
   process.env.PNEUMONIC,
   process.env.TEST_NETWORK
-  
-
 ); 
 const web3 = new Web3(provider);
 
@@ -17,23 +15,11 @@ const deploy = async () => {
   console.log("Attempting to deploy from account", accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({
-      data: bytecode,
-    })
-    .send({
-      gas: 1000000,
-      from: accounts[0],
-    });
+    .deploy({data: bytecode,})
+    .send({gas:' 1000000',from: accounts[0],});
 
   console.log(interface);
   console.log("Contract is deployed to", result.options.address);  //contract address
 };
 deploy();
 
-/*import Web3 from './web3';
-
-//window.ethereum.request({method:"eth_requestAccounts"});
-//const web3 = new Web3(Window.ehthereum);
-const web3 = new Web3(window.web3.currentProvider);
-
-export default web3;*/
